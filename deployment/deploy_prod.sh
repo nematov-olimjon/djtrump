87f2e6e47e646aba03beb8188904586291025dfc
nematov-olimjon <nematov.ol2001@gmail.com>
2022-10-26 18:24:20 +0000

    Git log fixed

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -6,6 +6,7 @@ ssh root@104.236.57.112 <<EOF
   source /opt/envs/djtrump/bin/activate
   pip install -r requirements.txt
   ./manage.py migrate
+  gunicorn --workers 3 --bind 0.0.0.0:8030 djtrump.wsgi --reload
   sudo supervisorctl restart djtrump
   exit
 EOF
