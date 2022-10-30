0759b3f0d23320f3a0749ef7641a2ccc3407e91f
Olimjon Nematov <69188755+nematov-olimjon@users.noreply.github.com>
2022-10-30 13:22:48 +0500

    Update deploy_prod.sh

--- deployment/deploy_prod.sh
+++ deployment/deploy_prod.sh
@@ -6,7 +6,7 @@ ssh ubuntu@184.73.131.84 <<EOF
   source /opt/envs/djtrump/bin/activate
   pip install -r requirements.txt
   ./manage.py migrate
-  gunicorn --workers 3 --bind 0.0.0.0:8030 djtrump.wsgi --reload
+  gunicorn --workers 3 --bind 0.0.0.0:8030 djtrump.wsgi --reload &
   sudo supervisorctl restart djtrump
   exit
 EOF
